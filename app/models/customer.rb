f84aaa9cd5b779d628b685e130ff82f921b3a7b4
EC2 Default User <ec2-user@ip-172-31-14-18.ec2.internal>
2021-10-18 10:34:05 +0000

    Add-routes-controller

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,10 +9,10 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :addresses, dependent: :destroy
 
-  validates :password, on: :create
+  validates :password, presence: true, on: :create 
   validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :last_name_kana, format:{with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana, format:{ with:/\A[ァ-ヶー－]+\z/}
   validates :first_name_kana, format:{with: /\A[ァ-ヶー－]+\z/}
   validates :postal_code, format:{with: /\A\d{7}\z/}
   validates :address, presence: true
