c52bf6d32de08867ebd3fdd0d1299791c0cdd410
KENCSK <poru330@gmail.com>
2021-10-19 06:24:08 +0000

    Add-rails5.2.6

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
 
   # 顧客用
   # URL /customers/sign_in ...
-  devise_for :customers,skip: [:passwords] , controllers: {
-  registrations: "customers/registrations",
-  sessions: 'customers/sessions'
+  devise_for :customers,skip: [:passwords,], controllers: {
+    registrations: "customers/registrations",
+    sessions: 'customers/sessions'
   }
 
   # 管理者用
   # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
+  devise_for :admin,skip: [:registrations, :passwords] ,controllers: {
+   sessions: "admin/sessions"
   }
 
 
