92b81412c4b9b59a178b50c4e472bb8026633a62
EC2 Default User <ec2-user@ip-172-31-9-32.ec2.internal>
2021-10-21 04:23:47 +0000

    admin_customer_controller_conflict

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,18 @@
 class Admin::CustomersController < ApplicationController
+<<<<<<< Updated upstream
+=======
+
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+>>>>>>> Stashed changes
 end
