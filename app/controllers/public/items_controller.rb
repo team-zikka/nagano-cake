8693159810ea002a94d3dd2f90970142c05e7e6a
EC2 Default User <ec2-user@ip-172-31-14-18.ec2.internal>
2021-10-21 08:51:42 +0000

    管理者側に商品登録ボタン追加、顧客側の商品画面作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,11 +3,12 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
-  
+
   def show
-    
+    @item = Item.find(params[:id])
+    @cart_item = CartItem.new
   end
-  
+
   private
     def item_params
       params.require(:item).permit(:genre_id, :name, :introduction, :price, :is_active, :image )
