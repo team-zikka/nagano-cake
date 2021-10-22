96b296336aad81650636a6981de2ed26f345c708
nsdakt <dnj.0077@gmail.com>
2021-10-22 11:15:06 +0000

    カート機能追加時コンフリクト解消

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,11 @@
 class Public::OrdersController < ApplicationController
+
+  def new
+    @customer = current_customer
+  end
+
+  def confirm
+    @item = Item.find(params[:item_id])
+    @order_item = @item.oeder.new(order_params)
+  end
 end
